e37290d60ca6f6f1d87fda8f7ee3f0d62283df3e
Travis <travishill62102@gmail.com>
2023-09-21 18:08:55 -0400

    added service selection process

--- serviceDisabler.py
+++ serviceDisabler.py
@@ -1,11 +1,25 @@
 import subprocess
-import sys
-# pip install psutil
-import psutil
+import random
 
+
+def get_services():
+    output = subprocess.Popen(["systemctl", "--type=service", "--state=running"], stdout=subprocess.PIPE)
+    output = output.stdout.read().decode()
+    return output
+
+def select_service(list_of_services_string):
+    total_services = len(list_of_services_string)
+    # Selects service randomly by line. Exclude 0 to skip headers
+    service_number = random.randrange(1, total_services)
+    service_list = list_of_services_string.split()
+    selected_service_string = service_list[service_number]
+    service_parts = selected_service_string.split()
+    service_name = service_parts[0]
+    return service_name
+    
 def main():
-    cmd = 'systemctl --type=service --state=running'
-    output = subprocess.Popen(cmd)
-    print(output)
+    services = get_services()
+    service = select_service(services)
+    print(service)
 
 main()
